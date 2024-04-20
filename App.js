ca6e04a789a166253fcae90e440e0909e7425cb6
Sanchitarastogi <sanchitarastogi03@gmai.com>
2024-04-20 07:22:04 -0400

    Initial Set up

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react/client";
+
+const AppLayout = () => {};
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
